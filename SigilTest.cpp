7753565db36f172f8258a05e0d8f51495505b52b
Tupac <tupacas1998@gmail.com>
2023-09-15 10:12:38 -0300

    Inicio del breakout

---
+++ SigilTest.cpp
@@ -0,0 +1,29 @@
+#include "sl.h"
+#include <iostream>
+
+using namespace std;
+
+
+int main()
+{
+
+	const int width = 800;
+	const int height = 400;
+
+	slWindow(width, height, "BreakOut", false);
+
+	while (!slShouldClose() && !slGetKey(SL_KEY_ESCAPE))
+	{
+			 
+		slSetBackColor(0, 0, 0);
+
+		slSetForeColor(1, 1, 1, 1);
+		slRectangleFill(width * 0.5, height * 0.05, 130, 15);
+
+		slRender();
+	}
+
+	slClose();
+
+	return 0;
+}
